8459b926a588eca521d576c257a3816e238adb70
KAVIDU-MDV2 <kaviduasehnmdb.ot@gmail.com>
2025-08-31 20:25:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'KAVIDU-MD=mooynSzY#_3Y-_5B__WCmeH459Dk8dQFfpLf2r-vYKdn3Gtt7wpc' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'KAVIDU-MD=lARAiAgC#m2rPXQSuyorMcJzRxFF1BBR-0Db2d-b08zUJXwZgtZo' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
 
